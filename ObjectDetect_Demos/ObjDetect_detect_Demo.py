27fbeccea6c75afb37d97daf4be6723f668f35e8
EpsilionJT <47164793+EpsilionJT@users.noreply.github.com>
2020-01-28 23:57:53 +0800

    Update ObjDetect_detect_Demo.py

--- ObjectDetect_Demos/ObjDetect_detect_Demo.py
+++ ObjectDetect_Demos/ObjDetect_detect_Demo.py
@@ -1,5 +1,6 @@
 from zisan.ObjDetect.Interface import ObjDetect_detect, ObjDetect_train, ObjDetect_Preprocess
 import os
+from PIL import Image
 
 '''
 Before running detect, you must keep the data path in the correct position
@@ -11,7 +12,9 @@ if __name__ == "__main__":
     current_path = os.path.dirname(__file__)
 
     detectModel=ObjDetect_detect(cfg='yolov3-tiny.cfg',currentpath=current_path)
-    re=detectModel.detect_fromfiles(images='D:/Users/JintuZheng/Testsamples',log_print=True) # images is the path where all the test images
+    #re=detectModel.detect_fromfiles(images='D:/tstImages',log_print=True) # images is the path where all the test images
+    img=Image.open('D:/1.jpg').convert('RGB')
+    re=detectModel.detect_from_RGBimg(img,is_showPreview=True)
 
     for img in re: # get one img classesNums
         print(img[1])
@@ -20,7 +23,7 @@ if __name__ == "__main__":
         print(img[0])
 
     #log_print true means you can read the results in the terminal
-
+    
     #re=[img0,img1,img2]
     #img0=[result_boxes,classesnum]
     #result_boxes=(x1,x2,y1,y2)   
